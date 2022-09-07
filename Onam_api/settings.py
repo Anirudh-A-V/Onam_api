c5bac010c1c9c8f44cb5052f0bd3fbf46dabb210
Anirudh A V <anirudh.av02@gmail.com>
2022-09-07 09:16:51 +0530

    STATICFILES_STORAGE added by whitenoise

--- Onam_api/settings.py
+++ Onam_api/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
